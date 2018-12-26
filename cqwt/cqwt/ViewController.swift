5ef6731910d7f2424ccb95e4f95c603c59ed663d
20161104578 <a20161104578@chaolimengdeiMac.local>
2018-12-26 10:31:00 +0800

    textx
    x

--- cqwt/cqwt/ViewController.swift
+++ cqwt/cqwt/ViewController.swift
@@ -11,93 +11,99 @@ import UIKit
 
 class ViewController: UIViewController {
     
-                                                                                                                                                                                                                                                                                                                                                                                                                                
+                                                            
     
-                        @IBOutlet var cityname: UILabel?
+            @IBOutlet var cityname: UILabel
     
-                    
+            
     
-                                        @IBOutlet var Temp: UILabel?
+            @IBOutlet var Temp: UILabel
     
-                  
+            
     
-                        override func viewDidLoad() {
+            override func viewDidLoad() {
         
-                                                                                                            super.viewDidLoad()
+                        super.viewDidLoad()
         
-                                                    
+                      
         
-                                                            var(city,temp)=json()
+                        var(city,temp)=json()
         
-         
+          
         
-                                    cityname.text=toString(city)+"市"
+                        cityname.text=toString(city)+"市"
         
-                                    Temp.text=toString(temp)+"c"
+                        Temp.text=toString(temp)+"c"
         
-               }
+                }
     
     
     
-           override func didReceiveMemoryWarning() {
+            override func didReceiveMemoryWarning() {
         
-                       super.didReceiveMemoryWarning()
+                        super.didReceiveMemoryWarning()
         
-                       // Dispose of any resources that can be recreated.
+                        // Dispose of any resources that can be recreated.
         
-               }
+                }
     
     
     
     
     
-           
+            
     
-           
+            
     
-           
+            
     
-           func json()->(NSObject,NSObject)
+            func json()->(NSObject,NSObject)
         
-           {
+            {
             
-                   
+                    
             
-                   var url=NSURL(string: "http://www.weather.com.cn/data/sk/101280601.html")
+                    var url=NSURL(string: "http://www.weather.com.cn/data/sk/101280601.html")
             
-                   
+                    
             
-                    var data=NSData(contentsOfURL: url as! URL)
+                    var data=NSData(contentsOfURL: url)
             
-                   
+                    
             
-                   
+                    
             
-                    var json:AnyObject!=JSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.AllowFragments, error: nil)
+                    var json:AnyObject!=NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.AllowFragments, error: nil)
             
-                   
+                    
             
-                    var weatherinfo:AnyObject=json.objectForKey("weatherinfo") as AnyObject
+                    var weatherinfo:AnyObject=json.objectForKey("weatherinfo")
             
-                   
+                    
             
-                   
+                    
             
-                    var city:NSObject=weatherinfo.objectForKey("city") as! NSObject
+                    var city:NSObject=weatherinfo.objectForKey("city") as NSObject
             
-                   
+                    
             
-                    var temp:NSObject=weatherinfo.objectForKey("temp") as! NSObject
+                    var temp:NSObject=weatherinfo.objectForKey("temp") as NSObject
             
-                   
+                    
             
-                   return(city,temp)
+                    return(city,temp)
             
-                   
-        
-                   }
+                    
+            
+                    }
     
-           
+            
     
 }
 
+
+---------------------
+作者：lanzeng_in_sz
+来源：CSDN
+原文：https://blog.csdn.net/lanzeng_in_sz/article/details/38351193
+版权声明：本文为博主原创文章，转载请附上博文链接！
